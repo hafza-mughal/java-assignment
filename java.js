34f19e9bf38a18d78fa1eee8d2c456bf7104a48a
hafza-mughal <156007447+hafza-mughal@users.noreply.github.com>
2024-04-26 08:14:41 -0700

    java assignment 1 is uploaded

---
+++ java.js
@@ -0,0 +1,9 @@
+ 
+
+ let firstname = prompt("enter your first name")
+
+ let middlename = prompt("enter your middle name")
+ 
+ let lastname = prompt("enter your last name")
+
+ console.log(firstname, middlename, lastname);
